572a09b9b86e01a2f88b6de15eb944b9269858e1
Eric Ding <ericdingg16@outlook.com>
2023-01-01 13:40:05 -0800

    mes

--- scripts/myspace.js
+++ scripts/myspace.js
@@ -3,3 +3,48 @@ function refreshTime() {
   document.getElementById("time").textContent = datetime;
 }
 setInterval(refreshTime, 1000);
+
+let list = document.querySelector('.messageList');
+let mesInput = document.querySelector('.messageInput');
+const submitButton = document.querySelector('button');
+
+
+let mesHis = [];
+const MAX_HIS = 5;
+
+if (localStorage.getItem('mesHisLocal')) {
+  mesHis = localStorage.getItem('mesHisLocal').split(',');
+  list.innerHTML = '';
+
+  for (const itemText of mesHis) {
+    const listItem = document.createElement('li');
+    listItem.textContent = itemText;
+    list.appendChild(listItem);
+  }
+
+  mesInput.value = '';
+  mesInput.focus();
+}
+
+submitButton.onclick = () => {
+  if (mesInput.value !== '') {
+    mesHis.unshift(mesInput.value);
+  }
+
+  if (mesHis.length > MAX_HIS) {
+    mesHis.pop();
+  }
+
+  localStorage.setItem('mesHisLocal', mesHis);
+
+  list.innerHTML = '';
+
+  for (const itemText of mesHis) {
+    const listItem = document.createElement('li');
+    listItem.textContent = itemText;
+    list.appendChild(listItem);
+  }
+
+  mesInput.value = '';
+  mesInput.focus();
+}
